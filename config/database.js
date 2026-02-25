449e3e7d24f860c3bd74a9893ab676bfc2f0f245
Poyyy414 <jpcambiado@gbox.edu.ph>
2026-02-25 20:51:50 +0800

    2nd commit fix database.js

--- config/database.js
+++ config/database.js
@@ -9,9 +9,12 @@ const pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    },
     max: 10,
     idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000
+    connectionTimeoutMillis: 10000
 });
 
 pool.connect()
